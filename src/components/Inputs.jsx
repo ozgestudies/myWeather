35d36e1a27639f43425faa4a03f1172803a491b0
ozgestudies <30935106+ozgestudies@users.noreply.github.com>
2024-04-22 21:42:41 +0300

    Add files via upload

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { UilSearch, UiLocationPoint } from '@iconscout/react-unicons'
+
+function inputs() {
+  return 
+    <div>inputs</div>;
+  
+}
+
+export default inputs;
